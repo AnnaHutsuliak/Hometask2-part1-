0f926b6fcc856bf841107e0b78fb65fb79bbeddf
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2020-01-05 23:14:55 +0200

    Create hometask2.java
    I tried to make the number 987 with the help of the some codes from the internet...but as there was int numer 2 = 0, I couldn't set the result of sub = number2-number1.. So I decided to push this part of code as the comment.

---
+++ hometask2.java
@@ -0,0 +1,25 @@
+/*package com.epam.hometask2;
+public class Hometask2 {
+    public static void main ( String [] arg){
+        int number1 = 789;
+        System.out.println (number1);
+        int number2=0;
+        while(number1 != 0){
+            number2 = number2*10 + number1%10;
+            number1 /= 10; }
+        System.out.println(number2);
+        int result = 987-789;
+        System.out.println(result);
+    }
+}
+*/
+package com.epam.hometask2;
+public class Hometask2 {
+    public static void main(String[] arg) {
+        int number1 = 789;
+        int number2 = 987;
+        int sub = number2 - number1;
+        System.out.println(sub);
+    }
+}
+
